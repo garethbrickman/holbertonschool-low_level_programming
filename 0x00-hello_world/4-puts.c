b9eb4614c3408c122f0783be8507252753c0f220
Gareth Brickman <977@holbertonschool.com>
2019-06-20 15:09:18 +0000

    Add 4. c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/**
+* int main main prototype to output strcpy using puts function
+* Parameter defines char of text string
+*
+* Long description: xxx
+* Return: value will be 0
+*/
+#include<stdio.h>
+/* prints text*/
+int main(void)
+/* declarations: char str1
+*
+*/
+{
+	char str1[50];
+	strcpy (str1, "Programming is like building a multilingual puzzle");
+	return (0);
+}
