5ae9746b2cc725c43ac920e3839bd8153240b204
Gareth Brickman <977@holbertonschool.com>
2019-06-20 03:57:23 +0000

    Added 4. c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *function main initial short description
+ *describes parameter and what main does
+ *Return: value will be 0
+ */
+
+int main(void)
+{
+/*declarations*/
+
+	char str1[15];
+	strcpy(str1, "Programming is like building a multilingual puzzle\n);
+return(0);
+}
