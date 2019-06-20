0419881e5c7ab6d4015c1625edcc3f30b5996066
Gareth Brickman <977@holbertonschool.com>
2019-06-20 18:56:12 +0000

    Add 5. c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/** this is a short  description for a program to print plain text */
+#include<stdio.h>
+/**
+ * main - takes unspecificed number of arguments and returns value on
+ * Description: longer things about the main function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
