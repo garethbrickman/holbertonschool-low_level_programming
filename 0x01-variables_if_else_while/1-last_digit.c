8610b202588f2b5a7a59c5edb79ca8394909c9ec
Gareth Brickman <977@holbertonschool.com>
2019-06-24 22:09:25 +0000

    Edit 1. C file with program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,27 +1,27 @@
-/** this is a short description for a program to print plain text/file size */
+/** this is a short description for a program */
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 /**
  * main - takes unspecificed number of arguments and returns value on
  * Description: longer things about the main function
  * Return: 0
  */
+
 int main(void)
 {
-	char c;
-	int i;
-	double d;
-	float f;
+	int n;
 
-	printf("Size of a char: %lu", (unsigned long)sizeof(c));
-	printf(" byte(s)\n");
-	printf("Size of an int: %lu", (unsigned long)sizeof(i));
-	printf(" byte(s)\n");
-	printf("Size of a long int: %lu", (unsigned long)sizeof(i));
-	printf(" byte(s)\n");
-	printf("Size of a long long int: %lu", (unsigned long)sizeof(d));
-	printf(" byte(s)\n");
-	printf("Size of a float: %lu", (unsigned long)sizeof(f));
-	printf(" byte(s)\n");
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("Last digit of %d is %d ", n, (n % 10));
+	if ((n % 10) > 5)
+		printf("and is greater than 5\n");
+	if ((n % 10) == 0)
+		printf("and is 0\n");
+	if ((n % 10) <= 6)
+		printf("and is less than 6 and not 0\n");
 
 	return (0);
 }
