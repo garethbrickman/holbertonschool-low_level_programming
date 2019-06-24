38fa5e4e27d0d2e808839e11cc0482e797ea8324
Gareth Brickman <977@holbertonschool.com>
2019-06-24 16:09:21 +0000

    Edit 0. C file with correct program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,25 @@
-/** this is a short description for a program to print plain text/file size */
+/** this is a short description for a program */
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 /**
  * main - takes unspecificed number of arguments and returns value on
  * Description: longer things about the main function
  * Return: 0
  */
+
 int main(void)
 {
-	char c;
-	int i;
-	double d;
-	float f;
+	int n;
 
-	printf("Size of a char: %lu", (unsigned long)sizeof(c));
-	printf(" byte(s)\n");
-	printf("Size of an int: %lu", (unsigned long)sizeof(i));
-	printf(" byte(s)\n");
-	printf("Size of a long int: %lu", (unsigned long)sizeof(i));
-	printf(" byte(s)\n");
-	printf("Size of a long long int: %lu", (unsigned long)sizeof(d));
-	printf(" byte(s)\n");
-	printf("Size of a float: %lu", (unsigned long)sizeof(f));
-	printf(" byte(s)\n");
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
 
 	return (0);
 }
