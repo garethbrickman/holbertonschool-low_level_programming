11ca62f594ecc3d26f3f8f281498fb07f5d21f3f
Gareth Brickman <977@holbertonschool.com>
2019-06-24 23:24:14 +0000

    Edit 2. c file with program

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,21 +7,9 @@
  */
 int main(void)
 {
-	char c;
-	int i;
-	double d;
-	float f;
+	char al;
 
-	printf("Size of a char: %lu", (unsigned long)sizeof(c));
-	printf(" byte(s)\n");
-	printf("Size of an int: %lu", (unsigned long)sizeof(i));
-	printf(" byte(s)\n");
-	printf("Size of a long int: %lu", (unsigned long)sizeof(i));
-	printf(" byte(s)\n");
-	printf("Size of a long long int: %lu", (unsigned long)sizeof(d));
-	printf(" byte(s)\n");
-	printf("Size of a float: %lu", (unsigned long)sizeof(f));
-	printf(" byte(s)\n");
-
-	return (0);
+	for (al = 'a'; al <= 'z'; al++)
+		putchar(al);
+	putchar('\n');
 }
