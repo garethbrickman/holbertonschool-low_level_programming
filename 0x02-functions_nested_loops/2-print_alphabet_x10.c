7aabd225a8635de4ab53c5c5004e977c60d3b979
Gareth Brickman <977@holbertonschool.com>
2019-06-25 15:13:27 +0000

    Edit 2. c file to remove excess characters on line 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * print_alphabet_x10 - takes unspecificed number of arguments and returns value
+ * print_alphabet_x10 - takes unspecificed number of arguments, returns value
  * Description: longer things about the print_alphabet_x10 function
  * Return: 0
  */
