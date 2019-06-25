eaf59ed7d31092a97452b7b50a58d64d8a0d4bb2
Gareth Brickman <977@holbertonschool.com>
2019-06-25 13:59:59 +0000

    Edit 0. c file to use one char at a time

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,6 +7,15 @@
  */
 int main(void)
 {
-	_putchar("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
