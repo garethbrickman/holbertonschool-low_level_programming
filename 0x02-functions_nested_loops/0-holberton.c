712d46030d453ee2ec90d23aaffd6f6bc6d17fd8
Gareth Brickman <977@holbertonschool.com>
2019-06-25 13:44:08 +0000

    Edit 0. c file with comments

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,12 @@
+/** this is a short description for a program*/
 #include "holberton.h"
-int main(void);
-_putchar("Holberton\n");
-return (0);
+/**
+ * main - takes unspecificed number of arguments and returns value
+ * Description: longer things about the main function
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar("Holberton\n");
+	return (0);
+}
