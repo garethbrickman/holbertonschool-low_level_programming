7afe83bd0b1085d67feccadde72de8b18527c052
Gareth Brickman <977@holbertonschool.com>
2019-06-25 18:09:47 +0000

    Add 3. c files, edit holberton.h

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * function  _islower - takes unspecificed number of arguments, returns value
+ * Description: longer things about the int _islower(int c) function
+ * Return: 0
+ */
+int _islower(int c)
+{
+
+	if (c > 91)
+		return (1);
+
+	else
+		return (0);
+}
