cfb6c221f1819f47b2236fda88a48b8ab4aff9f8
Gareth Brickman <977@holbertonschool.com>
2019-06-27 21:51:45 +0000

    Add 4. c files, edit holberton.h

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 /**
- * print_numbers - function outputs digits 0-9
+ * print_most_numbers - function outputs digits 0-9 without 2 and 4
  *
  *
  * Return: 0
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int num;
 
 	for (num = 0; num < 10; num++)
+		if (num != 2 && num != 4)
 	{
 		_putchar(num + '0');
 	}
