4a8fd8ab6be3efb5628de8d74832a348352c9730
Gareth Brickman <977@holbertonschool.com>
2019-06-27 20:39:56 +0000

    Add 2. c files, edit holberton.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul - (int, int) function multiplies two ints
+ * @a: int for multiplication
+ * @b: int for multiplication
+ *
+ * Return: int mul
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
