25e08f371971d181bf73184e9c8f8b9adb1d4668
Gareth Brickman <977@holbertonschool.com>
2019-06-27 21:03:36 +0000

    Add 3. c files, _putchar.c, edit holberton.h

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_numbers - function outputs digits 0-9
+ *
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		_putchar(num + '0');
+	}
+		_putchar('\n');
+}
