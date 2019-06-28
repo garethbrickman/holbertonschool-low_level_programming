49bc75cd6dd44ec1019b3b5f1c018e3bf937e141
Gareth Brickman <977@holbertonschool.com>
2019-06-28 02:00:09 +0000

    Edit 6. c files

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,23 @@
 #include "holberton.h"
 /**
- * void print_line(int n) - function outputs _____
+ * print_line - function outputs _____
  *
- *@n is int for line
+ *@n: is int for line
  *
  * Return: 0
  */
+
 void print_line(int n)
 {
-	int num;
+	int ns;
 
-	for (num = 0; num < 10; num++)
-		if (num != 2 && num != 4)
+	for (ns = 0; ns < n; ns++)
 	{
-		_putchar(num + '0');
+		if (n <= 0)
+		{
+			_putchar ('\n');
+		}
+		_putchar (95);
 	}
-		_putchar('\n');
+			 _putchar ('\n');
 }
