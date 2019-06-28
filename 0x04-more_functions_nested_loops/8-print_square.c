5988d8373eeb8989dc76465eb4bf5cc9778b02c3
Gareth Brickman <977@holbertonschool.com>
2019-06-28 03:09:32 +0000

    Add 8. c files

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_square - function outputs # square
+ *
+ *@size: is int
+ *
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int row, col;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (col = 0; col < size; col++);
+		{
+			for (row = 0; row < size; row++);
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
