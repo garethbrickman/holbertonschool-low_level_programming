82f946afbe32b92cf74053cd2b954d522a17a5d5
Gareth Brickman <977@holbertonschool.com>
2019-06-28 23:02:06 +0000

    Edit 9. c file to remove spaces, add new line

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,23 +16,24 @@ int main(void)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf("Fizz Buzz ");
+			printf("Fizz Buzz");
 		}
 		else if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", n);
+			printf("%d", n);
 		}
 		if (n != 100)
 		{
 			printf(" ");
+			printf("\n");
 		}
 	}
 	return (0);
