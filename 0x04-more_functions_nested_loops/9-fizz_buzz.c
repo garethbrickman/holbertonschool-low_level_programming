65aa7a16892f87d6bb128e5f2432a8525e47e2fe
Gareth Brickman <977@holbertonschool.com>
2019-06-28 22:55:19 +0000

    Add 9. c file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - function outputs fizz buzz
+ *
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("Fizz Buzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		if (n != 100)
+		{
+			printf(" ");
+		}
+	}
+	return (0);
+}
