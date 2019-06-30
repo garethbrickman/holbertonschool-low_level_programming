e269d92b42852de722ae73d3b67546b01148aab7
Gareth Brickman <977@holbertonschool.com>
2019-06-30 18:11:30 +0000

    Edit 1. c file to 24 lines

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,7 +4,6 @@
  * main - causes an infinite loop
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
@@ -18,7 +17,7 @@ int main(void)
  *       {
  *               putchar(i);
  *       }
- */
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
