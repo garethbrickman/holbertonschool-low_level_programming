623e86edd0176e21f3841107bce87055e4728d0a
Gareth Brickman <977@holbertonschool.com>
2019-07-01 01:29:23 +0000

    Edit 8. c files

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,21 +7,13 @@
  */
 int main(void)
 {
-	char c;
-	int i;
-	double d;
-	float f;
+	int al, an;
 
-	printf("Size of a char: %lu", (unsigned long)sizeof(c));
-	printf(" byte(s)\n");
-	printf("Size of an int: %lu", (unsigned long)sizeof(i));
-	printf(" byte(s)\n");
-	printf("Size of a long int: %lu", (unsigned long)sizeof(i));
-	printf(" byte(s)\n");
-	printf("Size of a long long int: %lu", (unsigned long)sizeof(d));
-	printf(" byte(s)\n");
-	printf("Size of a float: %lu", (unsigned long)sizeof(f));
-	printf(" byte(s)\n");
+	for (al = '0'; al <= '9'; al++)
+		putchar(al);
+	for (an = 65; an <= 70; an++)
+		putchar(an);
+	putchar('\n');
 
 	return (0);
 }
