49ff8b5415d347a1a4ee0427881477b03cf1ff29
Gareth Brickman <977@holbertonschool.com>
2019-07-05 18:55:23 +0000

    Edit 4. c file function comments

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,18 +8,6 @@
  * Return: 0
  */
 
-int _strlen(char *s)
-{
-	char *copy_s = s;
-	int length = 0;
-
-	while (*copy_s != '\0')
-	{
-		length++;
-		copy_s++;
-	}
-	return (length);
-}
 
 void print_rev(char *s)
 {
@@ -32,4 +20,26 @@ void print_rev(char *s)
 		count--;
 	}
 	_putchar('\n');
+}
+
+/**
+ * _strlen - function returns length of a string
+ *
+ *@s: char pointer
+ *
+ * Return: length
+ *
+ */
+
+int _strlen(char *s)
+{
+	char *copy_s = s;
+	int length = 0;
+
+	while (*copy_s != '\0')
+	{
+		length++;
+		copy_s++;
+	}
+	return (length);
 }
