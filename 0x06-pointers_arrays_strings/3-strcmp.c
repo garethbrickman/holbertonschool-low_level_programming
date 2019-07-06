03d0e70a998c7830502811c70fc12e9bf8038413
Gareth Brickman <977@holbertonschool.com>
2019-07-06 23:21:41 +0000

    Edit 3. c files, add 101. c file, edit holberton.h

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * *_strcmp - function compares two strings
+ *
+ *@s1: char pointer
+ *@s2: char pointer
+ *
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+		int i;
+		for (
+}
+
+/**
+ * _strlen - function returns length of a string
+ *
+ *@s: char pointer
+ *
+ * Return: length
+ *
+ */
+
+int _strlen(char *s)
+{
+	char *copy_s = s;
+	int length = 0;
+
+	while (*copy_s != '\0')
+	{
+		length++;
+		copy_s++;
+	}
+	return (length);
+}
