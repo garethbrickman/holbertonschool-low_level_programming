44a09253db62b1540630136cda2e2f43b043be18
Gareth Brickman <977@holbertonschool.com>
2019-07-09 20:33:38 +0000

    Edit 4. c file

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * *_strpbrk - function searches string for set of bytes
+ *
+ *@s: char * pointer
+ *@accept: char * pointer
+ * Return: s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *ap = accept;
+	int counter = 0;
+
+	while (*s)
+	{
+		while (*(ap + counter))
+		{
+			if (*s == *(ap + counter))
+		{
+			return (s);
+		}
+			counter++;
+		}
+		counter = 0;
+		s++;
+	}
+	if (*accept == '\0')
+		return (s);
+	return (0);
+}
