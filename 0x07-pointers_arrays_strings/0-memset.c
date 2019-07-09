4e52eb6017ef22f6186b1a1af93316292897246a
Gareth Brickman <977@holbertonschool.com>
2019-07-09 18:54:26 +0000

    Add 0,1. c files, edit 2. c file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * *_memset - function fills memory with constant byte
+ *
+ *@s: char * pointer
+ *@b: char constant byte
+ *@n: unsigned int of bytes
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * *_memcpy - function copies memory area
+ *
+ *@dest: char * pointer
+ *@src: char * pointer
+ *@n: unsigned int
+ * Return: s
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	
+}
