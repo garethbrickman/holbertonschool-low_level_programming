bd8bd06edc4dcf9ba2cea1bd1b92e48d0b52ed4c
Gareth Brickman <977@holbertonschool.com>
2019-07-10 18:05:22 +0000

    Add 0. c file, _putchar.c, holberton.h and README

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - function prints a string
+ *
+ *@s: char * pointer
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
