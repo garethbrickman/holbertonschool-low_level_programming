36279f53f732976e5e4664f6753fe4edb6e8ec80
Gareth Brickman <977@holbertonschool.com>
2019-07-10 01:24:18 +0000

    Edit 0,1. c files

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,5 +11,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	
+	unsigned int counter = 0;
+
+	while (counter < n)
+	{
+		*(s + counter) = b;
+		counter++;
+	}
+	return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,11 +5,18 @@
  *
  *@dest: char * pointer
  *@src: char * pointer
- *@n: unsigned int
- * Return: s
+ *@n: unsigned int of bytes
+ * Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	
+	unsigned int counter = 0;
+
+	while (counter < n)
+	{
+		*(dest + counter) = *(src + counter);
+		counter++;
+	}
+	return (dest);
 }
