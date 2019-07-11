7e4c025d3b8ec9242d4981bd171e7ba09eba8803
Gareth Brickman <977@holbertonschool.com>
2019-07-11 21:37:29 +0000

    Edit 5. c file, add 6 c. file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,27 +10,26 @@
 
 int _sqrt_recursion(int n)
 {
-	if (y < 0)
-		return (-1);
-	else if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, --y));
+	return (factor(n, 1));
+
 }
 
 /**
- * _pow_recursion - function returns value of x ^ y
+ * factor - function checks if n is a square root
  *
+ *@n: integer number
  *@x: integer number
- *@y: integer number
  *
- * Return: -1 or power
+ * Return: x  or -1
  */
 
-int _pow_recursion(int x, int y)
+int factor(int n, int x)
 {
-	if (y < 0)
+	if (x * x < n)
+		return (factor(n, x + 1));
+	else if (x * x > n)
 		return (-1);
-	else if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, --y));
+	else if (x * x == n)
+		return (x);
+	return (0);
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - function returns 1 if int is prime
+ *
+ *@n: integer number
+ *
+ * Return: 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	return (factor(n, 2));
+
+}
+
+/**
+ * int factor - checks if number is prime
+ *
+ *@n: integer number
+ *@x: integer number
+ *
+ * Return: x  or -1
+ */
+
+int factor(int n, int x)
+{
+	if (n % x = 0)
+		return (0);
+	else if (n % x < 0)
+		
+
+x > (n / 2)
+	    return (0);
+	    else if (x < (n / 2)
+		     return 
+}
