824421f39378f8c2c4b651acaf0697f3dff27c00
Gareth Brickman <977@holbertonschool.com>
2019-07-14 01:16:43 +0000

    Add 3. c file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include<stdio.h>
+/**
+ * main - function multiplies two numbers
+ *
+ *@argc: int
+ *@argv: char * pointer to array
+ *
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int multi = 0;
+
+	if (argc == 3)
+	{
+		multi += atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", multi);
+	}
+	else
+	{
+		printf("Error\n");
+		return(1);
+	}
+	return (0);
+}
