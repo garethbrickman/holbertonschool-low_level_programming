15214adb2708356b9e05d5ef6b185723cebe7fae
Gareth Brickman <977@holbertonschool.com>
2019-07-14 00:07:52 +0000

    Add 1. c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include<stdio.h>
+/**
+ * main - function prints number of args passed in it
+ *
+ *@argc: int
+ *@argv: char * pointer to array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
