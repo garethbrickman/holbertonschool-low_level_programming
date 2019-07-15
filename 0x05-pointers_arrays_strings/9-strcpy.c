81f8820a6dbe797a841d5a29a7b888b8932e5ad6
Gareth Brickman <977@holbertonschool.com>
2019-07-15 20:03:00 +0000

    Edit 9. c file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,12 +12,12 @@
 char *_strcpy(char *dest, char *src)
 {
 	char *pdest = dest;
-	char *src = src1 - 1;
+	char *src1 = src - 1;
 
-	while (*src != '\0')
+	while (*src1 != '\0')
 	{
 		src1++;
-		*pdest = *src;
+		*pdest = *src1;
 		pdest++;
 	}
 	return (dest);
