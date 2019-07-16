68ccbf074556edb454e016ff8cd4ea1676d37168
Gareth Brickman <977@holbertonschool.com>
2019-07-16 20:41:20 +0000

    Add 1. c files

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * *_strdup - function returns ptr to newly allocated pace in memory, which
+ * contains a copy of the string given as a parameter.
+ *
+ *@str: char pointer
+ *
+ *
+ * Return: pstr2 or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *pstr = str;
+	char *pstr2;
+	char *pstr22;
+	int len = 0;
+	int mallen;
+
+	while (*pstr != '\0')
+	{
+		len++;
+		pstr++;
+	}
+	mallen = len + 1;
+
+	pstr2 = malloc(sizeof(char) * mallen);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		pstr22 = pstr2;
+
+		while (*str != '\0')
+		{
+			*pstr2 = *str;
+			str++;
+			pstr2++;
+		}
+		*pstr2 = *str;
+		return (pstr22);
+	}
+}
