28dad0be18305b470f62c9a74b3aeb3033a72d16
Gareth Brickman <977@holbertonschool.com>
2019-07-18 19:06:06 +0000

    Edit 1. c file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -49,11 +49,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			pstr[s1len + x] = s2[x];
 		}
 		pstr[s1len + x] = '\0';
-		if (!pstr)
-		{
-			return (NULL);
-		}
-		else
+		if (pstr)
 		{
 			return (pstr);
 		}
