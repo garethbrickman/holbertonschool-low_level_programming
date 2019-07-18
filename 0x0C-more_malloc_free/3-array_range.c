dee915724db19a2dc2f2132f4d48ecec911a3411
Gareth Brickman <977@holbertonschool.com>
2019-07-18 22:11:10 +0000

    Edit 3. c file

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	array = malloc(sizeof(int) * size);
+	array = malloc(sizeof(int) * (size + 1));
 	if (array == NULL)
 		return (NULL);
 	if (size > min)
