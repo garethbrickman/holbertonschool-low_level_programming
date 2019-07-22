154a9e2f60ab683082a041a03cf67089aea396ce
Gareth Brickman <977@holbertonschool.com>
2019-07-22 22:19:38 +0000

    Edit dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
+/*Prototypes*/
+
+(struct dog *d, char *name, float age, char *owner);
+
+/*Structs*/
 
 /**
  * struct dog - contains data for dog
@@ -17,7 +22,4 @@ struct dog
 
 /* Typdefs */
 
-/*Prototypes*/
-
-(struct dog *d, char *name, float age, char *owner);
 #endif
