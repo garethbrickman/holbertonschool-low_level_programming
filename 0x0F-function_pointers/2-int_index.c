45e071401a0f0084def26ae8a34fa85614cd88ac
Gareth Brickman <977@holbertonschool.com>
2019-07-24 23:26:58 +0000

    Edit 2. c file

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,20 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
+	if (size <= 0)
+		return (-1);
 	if (cmp)
 	{
-		
+		if (array)
+		{
+			for (i = 0; i < size; i++)
+			{
+				cmp(*(array + i));
+				return (i);
+			}
+		}
+	}
+	return (-1);
 }
