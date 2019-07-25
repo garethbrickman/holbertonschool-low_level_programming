b1740456afb8e7ec1c65134067ea32d4e25c858d
Gareth Brickman <977@holbertonschool.com>
2019-07-25 22:44:27 +0000

    Add 4. c file

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - function prints the opcodes of its own main function
+ *
+ *@argc: int
+ *@argv * pointer
+ *
+ * Return: 0
+ */
+
+int main (int argc, **char argv)
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (argv[1] < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+}
