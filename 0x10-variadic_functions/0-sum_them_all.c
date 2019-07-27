138a0221ef9277098da5e77735421ca117a24fea
Gareth Brickman <977@holbertonschool.com>
2019-07-27 18:00:57 +0000

    Edit 0. c file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,12 +18,12 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	va_start (list, n);
+	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(list, int);
 	}
-	va_end (list);
+	va_end(list);
 	return (sum);
 }
