9c7b2a255356b2ed63d722673bc489ce6f868c1f
Gareth Brickman <977@holbertonschool.com>
2019-08-05 20:20:03 +0000

    Add 1. c files

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - function returns number of elements in listint_t list
+ *
+ *@h: const listint_t pointer to head of list
+ *
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int x;
+
+	for (x = 0; h != NULL; x++)
+	{
+			h = h->next;
+	}
+	return (x);
+}
