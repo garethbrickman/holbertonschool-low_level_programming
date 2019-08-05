b7419b4c3fa128278657cd687201022544e7068f
Gareth Brickman <977@holbertonschool.com>
2019-08-05 19:16:45 +0000

    Add 0. c files, header, _putchar and README

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - function prints all elements of listint_t list
+ *
+ *@h: const listint_t pointer to head of list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int x;
+
+	for (x = 0; h != NULL; x++)
+	{
+			printf("%i\n", h->n);
+			h = h->next;
+	}
+	return (x);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is a README for my 0x13-more_singly_linked_lists project!
