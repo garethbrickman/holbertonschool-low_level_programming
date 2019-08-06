b87aaf481983b2749cdb4c5f12f35da7422d1be7
Gareth Brickman <977@holbertonschool.com>
2019-08-06 20:47:15 +0000

    Edit 5. c file for head conditionals

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,15 +13,14 @@ void free_listint2(listint_t **head)
 	listint_t *node = *head;
 	listint_t *temp;
 
-	if (head == NULL)
+	if (!head)
 		return;
-	if (*head == NULL)
-		return;
-			while (node != NULL)
-			{
-				temp = node;
-				node = node->next;
-				free(temp);
-				head = NULL;
-			}
+	while (*head)
+		while (node)
+		{
+			temp = node;
+			node = node->next;
+			free(temp);
+			*head = NULL;
+		}
 }
