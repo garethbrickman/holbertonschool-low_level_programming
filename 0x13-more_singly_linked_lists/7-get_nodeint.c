0549fe1ef12d7477f5ef831061f0fa81b3c1528b
Gareth Brickman <977@holbertonschool.com>
2019-08-06 17:47:02 +0000

    Add 7. c files

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * *get_nodeint_at_index - function deletes head node of listint_t list
+ *
+ *@head: pointer to pointer of head of linked list
+ *@index: index of node starting at 0
+ *
+ * Return: nth node of listint_t list, or NULL
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current = head;
+	unsigned int i = 0;
+
+	while (current != NULL)
+	{
+		if (i == index)
+			return (current);
+		i++;
+		current = current->next;
+	}
+	return (NULL);
+}
