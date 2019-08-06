c1d8adb9ed1030d98e536d684742abd947f46d18
Gareth Brickman <977@holbertonschool.com>
2019-08-06 20:20:13 +0000

    Edit 9. c file for NULL exceptions

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *currentnode = *head;
 	listint_t *newnode =  malloc(sizeof(listint_t));
 
-	while (head)
+	while (*head)
 	{
 		newnode->n = n;
 
