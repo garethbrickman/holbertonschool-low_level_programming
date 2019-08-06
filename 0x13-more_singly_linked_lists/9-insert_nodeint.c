bac6feb5b13c00ac4ee1c16f170dddd2a4300474
Gareth Brickman <977@holbertonschool.com>
2019-08-06 20:12:13 +0000

    Edit 9. c file for NULL cases

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,44 +14,27 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *currentnode = *head;
 	listint_t *newnode =  malloc(sizeof(listint_t));
-	newnode->n = n;
 
-	if (head == NULL)
-		return (newnode);
-	if (idx == 0)
+	while (head)
 	{
-		newnode->next = NULL;
+		newnode->n = n;
+
+		if (idx == 0)
+		{
+			newnode->next = NULL;
+			return (newnode);
+		}
+		while (currentnode->next != NULL && idx - 1 > 0)
+		{
+			currentnode = currentnode->next;
+			idx--;
+		}
+		if (currentnode->next == NULL)
+			return (NULL);
+
+		newnode->next = currentnode->next;
+		currentnode->next = newnode;
 		return (newnode);
 	}
-	while (currentnode->next != NULL && idx - 1 > 0)
-	{
-		currentnode = currentnode->next;
-		idx--;
-	}
-	if (currentnode->next == NULL)
-		return (NULL);
-
-	newnode->next = currentnode->next;
-	currentnode->next = newnode;
-	return (newnode);
+	return (NULL);
 }
-/* 	listint_t *temp = *head; */
-/* 	listint_t *newnode = NULL; */
-/* 	unsigned int i = 0; */
-
-/* 	while (temp != NULL) */
-/* 	{ */
-/* 		if (i == idx) */
-/* 		{ */
-/* 			newnode = malloc(sizeof(listint_t)); */
-/* 			if (newnode == NULL) */
-/* 				return (NULL); */
-/* 			newnode->n = n; */
-/* 			i++; */
-/* 			newnode->next = temp->next; */
-/* 			temp->next = newnode; */
-/* 			return (newnode); */
-/* 		} */
-/* 	} */
-/* 	return (NULL); */
-/* } */
