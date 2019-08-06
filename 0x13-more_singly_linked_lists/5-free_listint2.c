3dfadf6aa57a09f91e71ba2007fa037786f29596
Gareth Brickman <977@holbertonschool.com>
2019-08-06 20:42:01 +0000

    Edit 5. c file for header return conditions

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,9 +13,9 @@ void free_listint2(listint_t **head)
 	listint_t *node = *head;
 	listint_t *temp;
 
-	if (*head == NULL)
-		return;
 	if (head == NULL)
+		return;
+	if (*head == NULL)
 		return;
 			while (node != NULL)
 			{
