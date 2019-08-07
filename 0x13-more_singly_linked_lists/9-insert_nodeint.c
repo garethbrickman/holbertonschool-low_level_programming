2d190aab7ac16d23c7d410d7b6fcef77679c0569
Gareth Brickman <977@holbertonschool.com>
2019-08-07 19:32:27 +0000

    Edit 9. c file to change order of struct definitions

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,10 +13,11 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *currentnode = NULL;
-	listint_t *newnode =  malloc(sizeof(listint_t));
+	listint_t *newnode = NULL;
 
 	while (*head)
 	{
+		newnode = malloc(sizeof(listint_t));
 		currentnode = *head;
 		newnode->n = n;
 
