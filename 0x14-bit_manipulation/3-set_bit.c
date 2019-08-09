0f06140592a28f73333b3ecebb75de93cbc5a8dd
Gareth Brickman <977@holbertonschool.com>
2019-08-09 00:21:06 +0000

    Edit 3. c file for return -1 edge case

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,5 +14,8 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int mask = 1 << index;
 
 	*n = (*n & ~mask) | ((1 << index) & mask);
-	return (1);
+	if (index < 64)
+		return (1);
+	else
+		return (-1);
 }
