4c0a092baaa5b7be7fd7fec71641650cae153af9
Gareth Brickman <977@holbertonschool.com>
2019-08-09 00:19:18 +0000

    Add 3. c files

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * set_bit - function sets the value of a bit to 1 at a given index
+ *
+ *@n: unsigned long int ptr number input
+ *@index: unsigned int index position
+ *
+ * Return: 1 if successful, -1 if error
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1 << index;
+
+	*n = (*n & ~mask) | ((1 << index) & mask);
+	return (1);
+}
