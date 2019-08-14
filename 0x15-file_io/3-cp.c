e32b106c084b748b7c885d5e1932ffa12e2d06d9
Gareth Brickman <977@holbertonschool.com>
2019-08-14 18:47:05 +0000

    Edit 3. c file for betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,25 +21,33 @@ int main(int argc, char *argv[])
 	/*Open fd1 and fd2, error check*/
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file argv[1]\n"), exit(98);
-	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd2 = open(argv[2], O_CREAT | O_EXCL, 0664);
+	if (fd2 < 0)
+		fd2 = open(argv[2], O_TRUNC | O_WRONLY);
 	if (fd2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to argv[2]\n"), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	/*Loop until length of read return of fd1 == 0*/
 	while (lread)
 	{
 		lread = read(fd1, buff, sizeof(buff));
 		if (lread == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file argv[1]\n"), exit(98);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		lwrite = write(fd2, buff, lread);
 		if (lwrite == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to argv[2]\n"), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		if (lwrite != lread)
-			dprintf(STDERR_FILENO, "Error: Can't write to argv[2]\n"), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (close(fd1) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd fd1"), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd1), exit(100);
 	if (close(fd2) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd fd2"), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd2), exit(100);
 	return (0);
 }
