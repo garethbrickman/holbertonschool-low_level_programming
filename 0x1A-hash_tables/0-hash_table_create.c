1556a1f5d17080984dd3b8ddad121ac58b060b0a
Gareth Brickman <977@holbertonschool.com>
2019-10-10 20:18:43 +0000

    Edit 0. for betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -22,7 +22,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_table->array == NULL)
 	{
 		free(new_table);
-		return(NULL);
+		return (NULL);
 	}
 	/* set entries to NULL to be able to search properly */
 	while (x < size)
