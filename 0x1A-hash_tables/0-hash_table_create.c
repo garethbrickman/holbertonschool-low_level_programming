9c9aa10af0b1227869fbaee3dc1bde6b8b5aa0b5
Gareth Brickman <977@holbertonschool.com>
2019-10-10 20:11:44 +0000

    Edit 0. for betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,6 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	/* allocate memory for hash table, check if NULL */
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+
 	if (new_table == NULL)
 		return (NULL);
 
