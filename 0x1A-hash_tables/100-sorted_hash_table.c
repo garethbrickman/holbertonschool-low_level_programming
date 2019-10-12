a8d3ba17005cb9d6ae391a8c6d59dd7f7b825fc0
Gareth Brickman <977@holbertonschool.com>
2019-10-12 01:52:06 +0000

    Edit 100., header for struct conformity

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -12,14 +12,14 @@ shash_table_t *shash_table_create(unsigned long int size)
 	unsigned long int x = 0;
 
 	/* allocate memory for hash table, check if NULL */
-	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	shash_table_t *new_table = malloc(sizeof(shash_table_t));
 
 	if (new_table == NULL)
 		return (NULL);
 	if (size < 1)
 		return (NULL);
 	/* allocate memory for  entries in hash table */
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	new_table->array = malloc(sizeof(shash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
 		free(new_table);
@@ -48,7 +48,7 @@ shash_table_t *shash_table_create(unsigned long int size)
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *newnode;
+	shash_node_t *newnode;
 
 	if (strlen(key) < 1)
 		return (0);
@@ -86,7 +86,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
  * Return: void
  */
 
-void shash_table_print(const shash_table_t *ht);
+void shash_table_print(const shash_table_t *ht)
 {
 	unsigned int x;
 	int flick = 0;
@@ -96,7 +96,7 @@ void shash_table_print(const shash_table_t *ht);
 	printf("{");
 	for (x = 0; x < ht->size; x++)
 	{
-		hash_node_t *node = ht->array[x];
+		shash_node_t *node = ht->array[x];
 
 		if (node == NULL)
 		{
@@ -123,11 +123,11 @@ void shash_table_print(const shash_table_t *ht);
  * Return: void
  */
 
-void shash_table_delete(shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht)
 {
 	unsigned int x = 0;
-	hash_node_t *node = NULL;
-	hash_node_t *temp = NULL;
+	shash_node_t *node = NULL;
+	shash_node_t *temp = NULL;
 
 	for (; x < ht->size; x++)
 	{
@@ -143,4 +143,41 @@ void shash_table_delete(shash_table_t *ht);
 	}
 	free(ht->array);
 	free(ht);
+}
+
+/**
+ * shash_table_print_rev - function prints a hash table
+ *
+ * @ht: ptr to hash table
+ *
+ * Return: void
+ */
+
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	unsigned int x;
+	int flick = 0;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (x = 0; x < ht->size; x++)
+	{
+		shash_node_t *node = ht->array[x];
+
+		if (node == NULL)
+		{
+			continue;
+		}
+		if (flick == 1)
+		{
+			printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+		}
+		if (flick != 1)
+			printf("'%s': '%s'", node->key, node->value);
+		flick = 1;
+	}
+	printf("}");
+	printf("\n");
 }
