49f1e68d021296b5735bed2d967c47ea7424cc6f
Gareth Brickman <977@holbertonschool.com>
2019-10-12 01:41:55 +0000

    Edit header to comment out advanced prototypes

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -84,10 +84,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 /* Advanced Prototypes */
-shash_table_t *shash_table_create(unsigned long int size);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *ht);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);
+/* shash_table_t *shash_table_create(unsigned long int size); */
+/* int shash_table_set(shash_table_t *ht, const char *key, const char *value); */
+/* char *shash_table_get(const shash_table_t *ht, const char *key); */
+/* void shash_table_print(const shash_table_t *ht); */
+/* void shash_table_print_rev(const shash_table_t *ht); */
+/* void shash_table_delete(shash_table_t *ht); */
 #endif
