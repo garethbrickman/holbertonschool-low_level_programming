951ecb895677542064dfa2658fe97154fb6bab03
Gareth Brickman <977@holbertonschool.com>
2019-10-28 19:13:44 +0000

    Edit 0. for inner!=size-1 edge case

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -13,22 +13,25 @@ void bubble_sort(int *array, size_t size)
 	unsigned long int outer, inner;
 	int temp, swap;
 
-	for (outer = 0; outer < size; outer++)
+	if (size >= 2)
 	{
-		swap = 0;
-
-		for (inner = 0; inner < size - outer; inner++)
+		for (outer = 0; outer < size; outer++)
 		{
-			if (array[inner] > array[inner + 1])
+			swap = 0;
+
+			for (inner = 0; inner < size - outer; inner++)
 			{
-				temp = array[inner];
-				array[inner] = array[inner + 1];
-				array[inner + 1] = temp;
-				swap = 1;
+				if (array[inner] > array[inner + 1] && inner != size - 1)
+				{
+					temp = array[inner];
+					array[inner] = array[inner + 1];
+					array[inner + 1] = temp;
+					swap = 1;
+				}
 			}
+			if (!swap)
+				break;
+			print_array(array, size);
 		}
-		if (!swap)
-			break;
-		print_array(array, size);
 	}
 }
