453ab4b82292b645177cebbc5043d3940584d21d
Gareth Brickman <977@holbertonschool.com>
2019-10-28 22:43:45 +0000

    Edit 2. for print switch

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -11,22 +11,27 @@
 void selection_sort(int *array, size_t size)
 {
 	unsigned int outer, inner, min_index;
-	int temp;
+	int temp, flick;
 
 	if (size >= 2)
 	{
 		for (outer = 0; outer < size - 1; outer++)
 		{
+			flick = 0;
 			min_index = outer;
 			for (inner = outer + 1; inner < size; inner++)
 			{
 				if (array[inner] < array[min_index])
+				{
 					min_index = inner;
+					flick++;
+				}
 			}
 			temp = array[min_index];
 			array[min_index] = array[outer];
 			array[outer] = temp;
-			print_array(array, size);
+			if (flick)
+				print_array(array, size);
 		}
 	}
 }
