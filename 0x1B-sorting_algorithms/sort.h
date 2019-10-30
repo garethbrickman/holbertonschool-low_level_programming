2dad31998ce766cb80b0cf4ad55c289001440980
Gareth Brickman <977@holbertonschool.com>
2019-10-30 23:02:39 +0000

    Edit header

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -29,4 +29,9 @@ void quick_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 #endif
