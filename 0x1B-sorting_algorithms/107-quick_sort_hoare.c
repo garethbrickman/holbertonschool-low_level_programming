4abd3220bd3ca88da8f403391e14ca310cae0dc9
Gareth Brickman <977@holbertonschool.com>
2019-10-30 22:30:13 +0000

    Add 11. file, update header

---
+++ 0x1B-sorting_algorithms/107-quick_sort_hoare.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * quick_sort_hoare - function sorts array of ints using quick sort method
+ *
+ * @array: ptr to array
+ * @size: size of array
+ *
+ * Return: void
+ */
+
+void quick_sort_hoare(int *array, size_t size)
+{
+	int pivot = array[size - 1];
+	unsigned int i, j;
+	int temp;
+
+	if (size >= 2)
+	{
+		for (i = 0, j = size - 1;; i++, j--)
+		{
+			while (array[i] < pivot)
+				i++;
+			while (pivot < array[j])
+				j--;
+			if (i >= j)
+				break;
+
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+		quick_sort_hoare(array, i);
+		quick_sort_hoare(array + i, size - i);
+	}
+}
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -28,4 +28,5 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 #endif
