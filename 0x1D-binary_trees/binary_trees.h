4daf326b18e93874dd00b74e382f510084c5bb8c
Gareth Brickman <977@holbertonschool.com>
2019-12-05 20:22:14 +0000

    [FIX] Add 101 prototype

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -45,4 +45,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+/* Advanced */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+
 #endif
