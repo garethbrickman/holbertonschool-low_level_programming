b17ad791c2de285a848384fe2c593291a462e042
Gareth Brickman <977@holbertonschool.com>
2019-12-06 17:25:59 +0000

    [ADD] 18.

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -1,24 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle - Finds uncle of a given node
- * @node: ptr to node in binary tree
- *
- * Return: ptr to uncle node, or NULL if no uncle or no parent
+ * binary_tree_uncle - Finds uncle of node
+ * @node: node pointer
+ * Return: uncle node or NULL
  */
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	while (node)
-	{
-		if (!node->parent || !node->parent->parent)
-			return (NULL);
-
-		if (node == node->parent->left && node->parent->parent->left)
-			return (node->parent->parent->left);
-
-		if (node == node->parent->right && node->parent->parent->right)
-			return (node->parent->parent->right);
-	}
-	return (NULL);
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+	if (node->parent == node->parent->parent->right)
+		return (node->parent->parent->left);
+	return (node->parent->parent->right);
 }
