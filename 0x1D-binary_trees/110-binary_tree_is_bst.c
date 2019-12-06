d6403dcb42cf0fb66125a8a500a50252bf75cfef
Gareth Brickman <977@holbertonschool.com>
2019-12-06 18:15:46 +0000

    [FIX] Use min/max method instead

--- 0x1D-binary_trees/110-binary_tree_is_bst.c
+++ 0x1D-binary_trees/110-binary_tree_is_bst.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
-int bst_util(const binary_tree_t *tree, const binary_tree_t *prev);
+#include<limits.h>
+int bst_util(const binary_tree_t *tree, int min, int max);
 /**
  * binary_tree_is_bst - Checks if a tree is a BST
  * @tree: ptr to root node
@@ -9,34 +10,27 @@ int bst_util(const binary_tree_t *tree, const binary_tree_t *prev);
 
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	const binary_tree_t *prev = NULL;
-
 	if (!tree)
 		return (0);
 
-	return (bst_util(tree, prev));
+	return (bst_util(tree, INT_MIN, INT_MAX));
 }
 
 /**
  * bst_util - Checks if a tree is a BST
  * @tree: ptr to root node
- * @prev: ptr to previous node
+ * @min: minimum constraint
+ * @max: maximum constraint
  *
  * Return: 1 if is a BST, 0 if not
  */
 
-int bst_util(const binary_tree_t *tree, const binary_tree_t *prev)
+int bst_util(const binary_tree_t *tree, int min, int max)
 {
-	if (tree)
-	{
-		if (!bst_util(tree->left, prev))
-			return (0);
-		if (prev != NULL && tree->n <= prev->n)
-			return (0);
-
-		prev = tree;
-
-		return (bst_util(tree->right, prev));
-	}
-	return (1);
+	if (!tree)
+		return (1);
+	if (tree->n < min || tree->n > max)
+		return (0);
+	return (bst_util(tree->left, min, tree->n - 1) &&
+		bst_util(tree->right, tree->n + 1, max));
 }
