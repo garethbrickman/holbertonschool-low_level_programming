c9102d2341ea1852ee01c4faed86ff5b91822a24
Gareth Brickman <977@holbertonschool.com>
2020-01-14 20:55:41 +0000

    [ADD] Header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <ctype.h>
+#include <string.h>
+/* Structs */
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
