718ee1bd960ed65270fc27081a7a0e7d9f1a57d5
Gareth Brickman <977@holbertonschool.com>
2020-01-15 21:44:04 +0000

    [FIX] Semi-colon

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,7 +9,7 @@
 
 /* Prototypes */
 void print_array(int *array, size_t size);
-int recursive_bsearch(int *array, size_t size, int value, unsigned int index)
+int recursive_bsearch(int *array, size_t size, int value, unsigned int index);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif
