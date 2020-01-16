339eb85d47fca1b5a8a793ce625daa40d7301202
Gareth Brickman <977@holbertonschool.com>
2020-01-16 20:20:15 +0000

    [ADD] 104, header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,4 +12,5 @@ void print_array(int *array, size_t size);
 int recursive_bsearch(int *array, size_t size, int value, unsigned int index);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 #endif
